f9687d099cdc639868db7a6e187f1b2a87e55bda
Camila-RG <camilaramosgomes@gmail.com>
2025-02-09 09:13:08 -0300

    Interrupções adicionadas para função dos botões com debouncing

--- Atividade_Comunicacao_Serial.c
+++ Atividade_Comunicacao_Serial.c
@@ -9,6 +9,20 @@
 #include "inc/ssd1306.h"
 #include "inc/font.h"
 
+// Definição de constantes da matriz de leds
+#define IS_RGBW false
+#define NUM_PIXELS 25
+#define WS2812_PIN 7
+
+// Variáveis globais para armazenar a cor (Entre 0 e 255 para intensidade)
+uint8_t led_r = 255;
+uint8_t led_g = 0;
+uint8_t led_b = 127;
+
+// Variáveis de controle de estado
+static volatile int numero_atual = 0; // Número exibido na matriz de LEDs
+static uint32_t last_interrupt_time_a = 0; // Armazena o último tempo de interrupção do botão A
+static uint32_t last_interrupt_time_b = 0; // Armazena o último tempo de interrupção do botão B
 
 // Definição dos pinos dos leds e botões
 #define LED_G_PIN 11
@@ -41,11 +55,32 @@ void inicializacaocomponentes() {
     gpio_pull_up(BUTTON_B_PIN);
 }
 
+static void gpio_irq_handler(uint gpio, uint32_t events)
+{
+    uint32_t current_time = to_us_since_boot(get_absolute_time());
+
+    if (gpio == BUTTON_A_PIN && current_time - last_interrupt_time_a > 300000){ //Tempo de Debounce: 300 ms
+        last_interrupt_time_a = current_time;
+    }
+    else if (gpio == BUTTON_B_PIN && current_time - last_interrupt_time_b > 300000) { //Tempo de Debounce: 300 ms
+        last_interrupt_time_b = current_time;
+    }
+}
+
 int main()
 {
-    stdio_init_all();
-    inicializacaocomponentes();
+    PIO pio = pio0;
+    int sm = 0;
+    uint offset = pio_add_program(pio, &ws2812_program);
+    ws2812_program_init(pio, sm, offset, WS2812_PIN, 800000, IS_RGBW);
     
+    stdio_init_all();
+    inicializacaocomponentes(); //Inicialização dos pinos dos LEDs e botões
+    
+    // Configura interrupções para os botões
+    gpio_set_irq_enabled_with_callback(BUTTON_A_PIN, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
+    gpio_set_irq_enabled_with_callback(BUTTON_B_PIN, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
+
     while (true)
     {
     }
