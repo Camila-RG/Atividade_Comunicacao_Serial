8bb6a7aee52fa92a0cf4f179e33116a098a43fd6
Camila-RG <camilaramosgomes@gmail.com>
2025-02-09 08:30:50 -0300

    Bibliotecas adicionadas

--- Atividade_Comunicacao_Serial.c
+++ Atividade_Comunicacao_Serial.c
@@ -1,14 +1,53 @@
+// Bibliotecas necessárias para funcionamento
 #include <stdio.h>
+#include <stdlib.h>
 #include "pico/stdlib.h"
+#include "hardware/pio.h"
+#include "hardware/clocks.h"
+#include "ws2812.pio.h"
+#include "hardware/i2c.h"
+#include "inc/ssd1306.h"
+#include "inc/font.h"
 
 
+// Definição dos pinos dos leds e botões
+#define LED_G_PIN 11
+#define LED_B_PIN 12
+#define LED_R_PIN 13
+
+#define BUTTON_A_PIN 5
+#define BUTTON_B_PIN 6
+
+// Função para inicializar os LEDs e botões
+void inicializacaocomponentes() {
+    gpio_init(LED_G_PIN);
+    gpio_set_dir(LED_G_PIN, GPIO_OUT);
+    gpio_put(LED_G_PIN, 0);
+
+    gpio_init(LED_B_PIN);
+    gpio_set_dir(LED_B_PIN, GPIO_OUT);
+    gpio_put(LED_B_PIN, 0);
+
+    gpio_init(LED_R_PIN);
+    gpio_set_dir(LED_R_PIN, GPIO_OUT);
+    gpio_put(LED_R_PIN, 0);
+
+    gpio_init(BUTTON_A_PIN);
+    gpio_set_dir(BUTTON_A_PIN, GPIO_IN);
+    gpio_pull_up(BUTTON_A_PIN);
+
+    gpio_init(BUTTON_B_PIN);
+    gpio_set_dir(BUTTON_B_PIN, GPIO_IN);
+    gpio_pull_up(BUTTON_B_PIN);
+}
 
 int main()
 {
     stdio_init_all();
-
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+    inicializacaocomponentes();
+    
+    while (true)
+    {
     }
+    return 0;
 }
