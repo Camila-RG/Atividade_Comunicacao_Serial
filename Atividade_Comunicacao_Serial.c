b75081747746c735effec813912c5a2f7c62398a
Camila-RG <camilaramosgomes@gmail.com>
2025-02-09 21:30:10 -0300

    Reorganização do código fonte

--- Atividade_Comunicacao_Serial.c
+++ Atividade_Comunicacao_Serial.c
@@ -44,28 +44,7 @@ static uint32_t last_interrupt_time_b = 0;
 #define BUTTON_A_PIN 5
 #define BUTTON_B_PIN 6
 
-// Função para inicializar os LEDs e botões(Componentes)
-void inicializacaocomponentes() {
-    gpio_init(LED_G_PIN);
-    gpio_set_dir(LED_G_PIN, GPIO_OUT);
-    gpio_put(LED_G_PIN, 0);
-
-    gpio_init(LED_B_PIN);
-    gpio_set_dir(LED_B_PIN, GPIO_OUT);
-    gpio_put(LED_B_PIN, 0);
-
-    gpio_init(LED_R_PIN);
-    gpio_set_dir(LED_R_PIN, GPIO_OUT);
-    gpio_put(LED_R_PIN, 0);
-
-    gpio_init(BUTTON_A_PIN);
-    gpio_set_dir(BUTTON_A_PIN, GPIO_IN);
-    gpio_pull_up(BUTTON_A_PIN);
-
-    gpio_init(BUTTON_B_PIN);
-    gpio_set_dir(BUTTON_B_PIN, GPIO_IN);
-    gpio_pull_up(BUTTON_B_PIN);
-}
+void inicializacaocomponentes(); // Função para inicializar os LEDs e botões(Componentes)
 
 // Buffer para armazenar quais LEDs estão ligados matriz 5x5 e formar os números de 0 a 9
 bool led_buffer[10][NUM_PIXELS] = {
@@ -82,75 +61,20 @@ bool led_buffer[10][NUM_PIXELS] = {
     {1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1}, // Número 9
 };
  
-// Função para enviar dados para o LED WS2812
-static inline void put_pixel(uint32_t pixel_grb)
-{
-    pio_sm_put_blocking(pio0, 0, pixel_grb << 8u);
-}
- 
- // Função para converter valores RGB em um único valor de 32 bits no formato GRB
-static inline uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b)
-{
-    return ((uint32_t)(r) << 8) | ((uint32_t)(g) << 16) | (uint32_t)(b);
-}
- 
-// Função para acender um número específico na matriz de LEDs
-void set_one_led(uint8_t r, uint8_t g, uint8_t b, int numero) {
- 
-    uint32_t color = urgb_u32(r, g, b);
- 
-    for (int i = 0; i < NUM_PIXELS; i++) {
-        if (led_buffer[numero][i]) {
-            put_pixel(color);
-        } else {
-            put_pixel(0);
-        }
-    }
-}
+//Funções para enviar dados, converter valores e acender os LEDs
+
+static inline void put_pixel(uint32_t pixel_grb);
+static inline uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b);
+void set_one_led(uint8_t r, uint8_t g, uint8_t b, int numero);
 
 // Inicialização do display fora da interrupção
 static ssd1306_t ssd;
 
-// Inicializa o display
-void init_display() {
-    ssd1306_init(&ssd, WIDTH, HEIGHT, false, endereco, I2C_PORT);
-    ssd1306_config(&ssd);
-    ssd1306_fill(&ssd, false);
-    ssd1306_send_data(&ssd);
-}
+ // Inicializa o display
+void init_display();
 
 // Rotina de interrupção com debouncing dos botões
-static void gpio_irq_handler(uint gpio, uint32_t events)
-{
-    uint32_t current_time = to_us_since_boot(get_absolute_time());
-
-    if (gpio == BUTTON_A_PIN && current_time - last_interrupt_time_a > 300000) { // Deboucing
-        last_interrupt_time_a = current_time;
-
-    // Alterna o estado do LED Verde
-        bool estado = !gpio_get(LED_G_PIN);
-        gpio_put(LED_G_PIN, estado);
-
-        uart_puts(UART_ID, estado ? "LED Verde ligado\r\n" : "LED Verde desligado\r\n");
-
-        ssd1306_fill(&ssd, false);
-        ssd1306_draw_string(&ssd, estado ? "LED Verde ON" : "LED Verde OFF", 10, 30);
-        ssd1306_send_data(&ssd);
-    }
-    else if (gpio == BUTTON_B_PIN && current_time - last_interrupt_time_b > 300000) { // Deboucing
-        last_interrupt_time_b = current_time;
-
-        // Alterna o estado do LED Azul
-        bool estado = !gpio_get(LED_B_PIN);
-        gpio_put(LED_B_PIN, estado);
-
-        uart_puts(UART_ID, estado ? "LED Azul ligado\r\n" : "LED Azul desligado\r\n");
-
-        ssd1306_fill(&ssd, false);
-        ssd1306_draw_string(&ssd, estado ? "LED Azul ligado" : "LED Azul desligado", 10, 30);
-        ssd1306_send_data(&ssd);
-    }
-}
+static void gpio_irq_handler(uint gpio, uint32_t events);
 
 int main()
 {
@@ -161,25 +85,7 @@ int main()
     
     stdio_init_all();
     inicializacaocomponentes();
-
-    // I2C Inicialização
-    i2c_init(I2C_PORT, 400 * 1000);
-
-    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
-    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
-    gpio_pull_up(I2C_SDA);
-    gpio_pull_up(I2C_SCL);
-
     init_display();
-
-     // Inicializa a UART
-     uart_init(UART_ID, BAUD_RATE);
-
-     gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
-     gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
- 
-     const char *init_message = "Digite para exibir um caractere\r\n";
-     uart_puts(UART_ID, init_message);
 
     // Configura interrupções para os botões
     gpio_set_irq_enabled_with_callback(BUTTON_A_PIN, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
@@ -234,4 +140,114 @@ int main()
     sleep_ms(1000);
 
     return 0;
+}
+
+// Funções abaixo são para a matriz de LED RGB e botões
+
+void inicializacaocomponentes() {
+    gpio_init(LED_G_PIN);
+    gpio_set_dir(LED_G_PIN, GPIO_OUT);
+    gpio_put(LED_G_PIN, 0);
+
+    gpio_init(LED_B_PIN);
+    gpio_set_dir(LED_B_PIN, GPIO_OUT);
+    gpio_put(LED_B_PIN, 0);
+
+    gpio_init(LED_R_PIN);
+    gpio_set_dir(LED_R_PIN, GPIO_OUT);
+    gpio_put(LED_R_PIN, 0);
+
+    gpio_init(BUTTON_A_PIN);
+    gpio_set_dir(BUTTON_A_PIN, GPIO_IN);
+    gpio_pull_up(BUTTON_A_PIN);
+
+    gpio_init(BUTTON_B_PIN);
+    gpio_set_dir(BUTTON_B_PIN, GPIO_IN);
+    gpio_pull_up(BUTTON_B_PIN);
+
+     // I2C Inicialização
+     i2c_init(I2C_PORT, 400 * 1000);
+
+     gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
+     gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
+     gpio_pull_up(I2C_SDA);
+     gpio_pull_up(I2C_SCL);
+  
+    // Inicializa a UART
+      uart_init(UART_ID, BAUD_RATE);
+ 
+    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
+    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
+  
+    const char *init_message = "Digite para exibir um caractere\r\n";
+    uart_puts(UART_ID, init_message);
+}
+
+
+// Funções abaixo são para a matriz de LEDs ws2812
+
+static inline void put_pixel(uint32_t pixel_grb)
+{
+    pio_sm_put_blocking(pio0, 0, pixel_grb << 8u);
+}
+ 
+static inline uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b)
+{
+    return ((uint32_t)(r) << 8) | ((uint32_t)(g) << 16) | (uint32_t)(b);
+}
+ 
+void set_one_led(uint8_t r, uint8_t g, uint8_t b, int numero) {
+ 
+    uint32_t color = urgb_u32(r, g, b);
+ 
+    for (int i = 0; i < NUM_PIXELS; i++) {
+        if (led_buffer[numero][i]) {
+            put_pixel(color);
+        } else {
+            put_pixel(0);
+        }
+    }
+}
+
+// Funções do display ssd1306
+
+void init_display() {
+    ssd1306_init(&ssd, WIDTH, HEIGHT, false, endereco, I2C_PORT);
+    ssd1306_config(&ssd);
+    ssd1306_fill(&ssd, false);
+    ssd1306_send_data(&ssd);
+}
+
+// Funções de interrupções dos botões com debounce
+
+static void gpio_irq_handler(uint gpio, uint32_t events)
+{
+    uint32_t current_time = to_us_since_boot(get_absolute_time());
+
+    if (gpio == BUTTON_A_PIN && current_time - last_interrupt_time_a > 300000) { // Deboucing
+        last_interrupt_time_a = current_time;
+
+    // Alterna o estado do LED Verde
+        bool estado = !gpio_get(LED_G_PIN);
+        gpio_put(LED_G_PIN, estado);
+
+        uart_puts(UART_ID, estado ? "LED Verde ligado\r\n" : "LED Verde desligado\r\n");
+
+        ssd1306_fill(&ssd, false);
+        ssd1306_draw_string(&ssd, estado ? "LED Verde ON" : "LED Verde OFF", 10, 30);
+        ssd1306_send_data(&ssd);
+    }
+    else if (gpio == BUTTON_B_PIN && current_time - last_interrupt_time_b > 300000) { // Deboucing
+        last_interrupt_time_b = current_time;
+
+        // Alterna o estado do LED Azul
+        bool estado = !gpio_get(LED_B_PIN);
+        gpio_put(LED_B_PIN, estado);
+
+        uart_puts(UART_ID, estado ? "LED Azul ligado\r\n" : "LED Azul desligado\r\n");
+
+        ssd1306_fill(&ssd, false);
+        ssd1306_draw_string(&ssd, estado ? "LED Azul ligado" : "LED Azul desligado", 10, 30);
+        ssd1306_send_data(&ssd);
+    }
 }
